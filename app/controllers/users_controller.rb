308d227ce93670018517aae43a506ac2ae22267d
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-01 01:50:35 +0530

    Login page Added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,28 @@ class UsersController < ApplicationController
 			render('signup')
 		end
 	end
+	def login
+	end
+
+	def login_attempt
+		if params[:email].present? && params[:password].present?
+			@found_user = User.where(email: params[:email]).first
+			if @found_user
+				@au = @found_user.authenticate(params[:password])
+			else
+				flash[:notice] = "Incorrect Email"
+				render('login')
+			end
+		else
+			flash[:notice] = "Blank Fields"
+		end
+
+		if @au
+			flash[:notice] = "Successfully Logged In"
+			redirect_to root_path
+		else
+			flash[:notice] = "Incorrect Password"
+			render('login')
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get '/login'=>"users#login",as:"login"
+  post '/login'=>"users#login_attempt"
   resources :users;
   root 'users#signup'
     # The priority is based upon order of creation: first created -> highest priority.
