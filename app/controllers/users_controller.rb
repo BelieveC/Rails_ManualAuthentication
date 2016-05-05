7b64caa831d37865269068314b744c2e9af4c9ac
Chaitanya Yadav <cayadav2009@gmail.com>
2016-05-06 00:29:40 +0530

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(params.require(:user).permit(:email,:password))
 		if @user.save
-			redirect_to(root_path,notice:"Successfully Signed Up")
+			redirect_to(root_path,notice:"Successfully Signed Up!")
 		else
 			render('signup')
 		end
